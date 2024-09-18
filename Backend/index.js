f9f4722af3f47b060aebdfc1a3ebb618554f80bb
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2024-09-17 23:29:45 -0500

    Backend inicial

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
