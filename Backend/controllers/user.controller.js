2baa4f5931ec103ae4372bedf104beb2a63617a4
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2024-09-22 20:07:10 -0500

    Controladores de ruta "usuario" terminados completamente, falta probar el iniciar sesion del usuario con la contraseña hasheada

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -25,3 +25,71 @@ export const createUser = async (req, res) => {
         res.status(500).json({ message: "Error al crear el usuario", error });
     }
 }
+
+export const getUserbyEmail = async (req, res) => {
+    try {
+        const { email } = req.params;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error al obtener el usuario por email:', error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        const { email } = req.params;
+        const updateData = req.body;
+
+        const updatedUser = await User.findOneAndUpdate({ email }, updateData, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+
+        res.status(200).json({
+            message: "Usuario actualizado exitosamente",
+            user: updatedUser
+        });
+
+    } catch (error) {
+        console.error('Error al actualizar el usuario:', error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        const { email } = req.params;
+
+        const deletedUser = await User.findOneAndDelete({ email });
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+
+        res.status(200).json({
+            message: "Usuario eliminado exitosamente",
+            user: deletedUser
+        });
+    } catch (error) {
+        console.error('Error al eliminar el usuario:', error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, getUserbyEmail, getUsers, updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.post('/register', createUser);
+router.post('/register', createUser); // Se crea el usuario
+
+router.get('/:email', getUserbyEmail); // Se obtiene el usuario por email
+
+router.get('/', getUsers); // Se obtienen todos los usuarios
+
+router.put('/:email', updateUser); // Se actualiza el usuario por email
+
+router.delete('/:email', deleteUser); // Se elimina el usuario por email
 
 export default router;
