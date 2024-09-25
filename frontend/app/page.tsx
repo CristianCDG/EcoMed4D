cc049d7d0b42f0f717a1ec2d6748954a90eb0c13
Cristian Camilo Domínguez Gutiérrez <147455398+CristianCDG@users.noreply.github.com>
2024-09-25 11:24:06 -0500

    Se añadieron algunas funcionalidades responsive

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,12 +8,14 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <Hero />
       </div>
-      <div className="absolute top-0 right-0 w-1/2 h-full flex items-center justify-center">
-        <SignupFormDemo />
+      <div className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
+        <div className="w-full max-w-md mx-auto">
+          <SignupFormDemo />
+        </div>
       </div>
-      <div className="absolute top-0 right-0 w-full h-full flex items-center justify-center">
+      {/* <div className="absolute top-0 right-0 w-full h-full flex items-center justify-center">
         <SignInFormDemo />
-      </div>
+      </div> */}
     </main>
   );
 }
