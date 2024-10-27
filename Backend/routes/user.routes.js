8e8c78879e1ffcce53f69e3c0f4f1b94bb84f1a6
Cristian Camilo Domínguez Gutiérrez <147455398+CristianCDG@users.noreply.github.com>
2024-10-26 19:41:58 -0500

    EC4D-5: Funcionando envío de correos de verificación para el registro con token.

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,19 +1,17 @@
 import { Router } from "express";
-import { createUser, getUserbyEmail, getUsers, updateUser, deleteUser, loginUser } from "../controllers/user.controller.js";
+import { createUser, getUserbyEmail, getUsers, updateUser, deleteUser, loginUser, initiateRegistration, verifyEmail } from "../controllers/user.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
 
 const router = Router();
 
+router.post('/initiate-registration', initiateRegistration); // Iniciar el proceso de registro
+router.get('/verify-email', verifyEmail); // Verificar el token de correo electrónico
+
 router.post('/register', createUser); // Se crea el usuario
-
 router.get('/:email', getUserbyEmail); // Se obtiene el usuario por email
-
 router.get('/', getUsers); // Se obtienen todos los usuarios
-
 router.put('/:email', updateUser); // Se actualiza el usuario por email
-
 router.delete('/:email', deleteUser); // Se elimina el usuario por email
-
 router.post('/login', loginUser); // Ruta de inicio de sesión
 
 export default router;
