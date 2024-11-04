7d5ddfa6afda30fa973207700b8770afd41c05e3
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2024-11-03 23:03:00 -0500

    EC4D-36 Funcionalidad de enviar correo por medio del botón enviar
    Como los pacientes estan creados para juanmiguel, entonces metete a mi cuenta juanmiguelleon43@gmail.com password 1234 y pruebalo desde ahí, que sale el listado

--- Backend/routes/patient.routes.js
+++ Backend/routes/patient.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-
-import { createPatient, getPatients } from "../controllers/patient.controller.js";
-
+import { createPatient, getPatients, sendFileEmail } from "../controllers/patient.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import multer from 'multer';
 
 const router = Router();
+const upload = multer({ dest: 'uploads/' });
 
 router.get('/', authRequired, getPatients);
 router.post('/', authRequired, createPatient);
-
+router.post('/send-file', authRequired, upload.single('file'), sendFileEmail);
 
 export default router;
